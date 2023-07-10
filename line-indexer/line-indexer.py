4fd39f14dbd8c23479f6d9c53ffc72b1ae99f690
x1ao4 <112841659+x1ao4@users.noreply.github.com>
2023-07-10 19:41:52 +0800

    将 UTF-8 指定为默认编码方式

--- line-indexer/line-indexer.py
+++ line-indexer/line-indexer.py
@@ -1,3 +1,3 @@
-with open('input.txt', 'r') as input_file, open('output.txt', 'w') as output_file:
+with open('input.txt', 'r', encoding='utf-8') as input_file, open('output.txt', 'w', encoding='utf-8') as output_file:
     for i, line in enumerate(input_file, 1):
         output_file.write(f'{i};{line}')
